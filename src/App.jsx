e4c4e88961a8dbbaee775fe0f00d0222bd741614
Joao <gualberto.silvaj@gmail.com>
2026-07-29 13:36:11 -0400

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <main className="max-w-7x1 mx-auto">
+      <Navbar/>
+    </main>
+  )
+}
+export default App;
