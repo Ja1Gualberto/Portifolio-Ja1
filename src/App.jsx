4f697d92a1949efe03aaace51a95affe028d7962
Joao <gualberto.silvaj@gmail.com>
2026-07-30 14:08:01 -0400

    feat: Integra Three Fiber, adiciona a seção Hero, e troca de oxlint pra eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <main className="max-w-7x1 mx-auto">
       <Navbar/>
+      <Hero />
     </main>
   )
 }
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,53 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
+
+const Hero = () => {
+  const controls = useControls('HackerRoom', {
+    positionX: { value: 0, min: -10, max: 10 },
+    positionY: { value: 0, min: -10, max: 10 },
+    positionZ: { value: 0, min: -10, max: 10 },
+    rotationX: { value: 0, min: -10, max: 10 },
+    rotationY: { value: 0, min: -10, max: 10 },
+    rotationZ: { value: 0, min: -10, max: 10 },
+    scale: { value: 0.07, min: 0.01, max: 0.5 },
+  }
+  );
+  
+  return (
+    <section className="min-h-screen w-full flex flex-col">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center">Hi, I'm João <span className="waving-hand">🤘</span></p>
+        <p className="hero_tag text-gray_gradient">Building Web Products</p>
+      </div>
+      
+      <div className="w-full h-full absolute inset-0">
+        <Leva  />
+        <Canvas className="w-full h-full">
+          
+          <Suspense fallback={<CanvasLoader/>}>
+            
+            <PerspectiveCamera makeDefault position={[0,0,30]}/>
+            <HackerRoom
+              // scale={0.07}
+              // position={[0,0,0,]}
+              // rotation={[0, 280, 0]}
+              // scale={[controls.positionX, x.positionY, x.positionZ]}
+              position={[controls.positionX, controls.positionY, controls.positionZ]}
+              rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
+              scale={controls.scale}
+            />
+            
+            <ambientLight intensity={1} />
+            <directionalLight position={[10,10,10]}/>
+          </Suspense>
+          
+        </Canvas>
+      </div>
+    </section>
+  ); 
+}
+export default Hero;
