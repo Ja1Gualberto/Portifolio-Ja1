b757daf164f87e256b3bd1523c099d516679629c
Joao <gualberto.silvaj@gmail.com>
2026-08-03 13:54:38 -0400

    feat: Integra Modelo 3d(Laravel e REact)  na seção Hero com dimensionamneto responsivo e animação

--- src/constansts/index.js
+++ src/constansts/index.js
@@ -248,12 +248,13 @@ export const myProjects = [
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
-    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : isTablet ? 0.065 : 0.09,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
     cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    reactLogoPosition: isSmall ? [6, 4, 0] : isMobile ? [8, 4, 0] : isTablet ? [9, 4, 0] : [12, 3, 0],
     ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    targetPosition: isSmall ? [-8, -10, -10] : isMobile ? [-13, -10, -10] : isTablet ? [-12, -7, -10] : [-18, -13, -10],
+    laravelLogoScale: isSmall ? 3 : isMobile ? 4 : isTablet ? 5 : 7,
   };
 };
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,25 +4,35 @@ import HackerRoom from "../components/HackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader";
 import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constansts";
+import LaravelLogo from "../components/LaravelLogo.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
 
 const Hero = () => {
   const controls = useControls('HackerRoom', {
-    positionX: { value: 0, min: -10, max: 10 },
-    positionY: { value: 0, min: -10, max: 10 },
-    positionZ: { value: 0, min: -10, max: 10 },
-    rotationX: { value: 0, min: -10, max: 10 },
-    rotationY: { value: 0, min: -10, max: 10 },
-    rotationZ: { value: 0, min: -10, max: 10 },
-    scale: { value: 0.07, min: 0.01, max: 0.5 },
-  }
+      positionX: { value: 0, min: -10, max: 10 },
+      positionY: { value: 0, min: -10, max: 10 },
+      positionZ: { value: 0, min: -10, max: 10 },
+      rotationX: { value: 0, min: -10, max: 10 },
+      rotationY: { value: 0, min: -10, max: 10 },
+      rotationZ: { value: 0, min: -10, max: 10 },
+      scale: { value: 0.07, min: 0.01, max: 0.5 },
+    }
   );
+  const isSmall = useMediaQuery({maxWidth: 440});
+  const isMobile = useMediaQuery({maxWidth: 768});
+  const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+  
+  const tamanhos = calculateSizes(isSmall, isMobile, isTablet);
   
   return (
-    <section className="min-h-screen w-full flex flex-col">
+    <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center">Hi, I'm João <span className="waving-hand">🤘</span></p>
         <p className="hero_tag text-gray_gradient">Building Web Products</p>
       </div>
+      
       
       <div className="w-full h-full absolute inset-0">
         <Leva  />
@@ -32,14 +42,19 @@ const Hero = () => {
             
             <PerspectiveCamera makeDefault position={[0,0,30]}/>
             <HackerRoom
-              // scale={0.07}
-              // position={[0,0,0,]}
-              // rotation={[0, 280, 0]}
-              // scale={[controls.positionX, x.positionY, x.positionZ]}
-              position={[controls.positionX, controls.positionY, controls.positionZ]}
-              rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
-              scale={controls.scale}
+              scale={tamanhos.deskScale}
+              position={tamanhos.deskPosition} 
+              rotation={[0.1, -Math.PI, 0]}
+              
+              // position={[controls.positionX, controls.positionY, controls.positionZ]}
+              // rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
+              // scale={controls.scale}
             />
+            
+            <group>
+              <LaravelLogo position={tamanhos.targetPosition} scale={tamanhos.laravelLogoScale}/>
+              <ReactLogo position={tamanhos.reactLogoPosition} scale={tamanhos.deskScale}/>
+            </group>
             
             <ambientLight intensity={1} />
             <directionalLight position={[10,10,10]}/>
